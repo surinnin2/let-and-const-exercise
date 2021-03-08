5b68e3e1a22858cc2125fccd43e3b83a59b79317
Rob <surinnin2@gmail.com>
2021-03-07 19:16:23 -0500

    exercise completed

---
+++ letAndConst.js
@@ -0,0 +1,9 @@
+
+const PI = 3.14;
+
+/*
+1. Var can be redeclared with the same variable name while let cannot be redeclared. Let is also limited in scope to the block it was decalred in whereas var is limited in scope to the function.
+2. Const cannot be redeclared or have its value reassigned to another value unlike var. The scope remains in block as with let.
+3. Const cannot have its value reassigned while let can.
+4. Hoisting is the event where JS declares the variable before initializing the values of the variables with using var. Let and const does not allow this and the variable declaration occurs with the initialization of its values.
+*/
